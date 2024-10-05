f15af579a78370691a2566b510c7df76274a360e
Juan David Velez H <juandavidv4@gmail.com>
2024-10-04 21:24:22 -0500

    updates of versions of .net core to .net 8

---
+++ Music.Player.API/Controllers/MusicPlayerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Music.Player.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicPlayerController : ControllerBase
+    {
+    }
+}
